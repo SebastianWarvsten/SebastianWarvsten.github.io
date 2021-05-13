ef3683eee846c47338e533264dbce5dcb4f3e7d3
Sebastian_Warvsten <sebastian.warvsten@iths.se>
2021-05-13 17:01:54 +0200

    Inlämning av dist-filer

---
+++ precache-manifest.7eaded01ee3a26a174b63e9b31ec80f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f8d98b08cc2102d15e",
+    "url": "/css/app.d086ee78.css"
+  },
+  {
+    "revision": "e2a5586a09efba80688a",
+    "url": "/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "281c55cb6e93b5c080ea1f31d7420618",
+    "url": "/img/logo.281c55cb.png"
+  },
+  {
+    "revision": "51b90db90114e3d6f3378c2448cab251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f8d98b08cc2102d15e",
+    "url": "/js/app.1414a2c9.js"
+  },
+  {
+    "revision": "e2a5586a09efba80688a",
+    "url": "/js/chunk-vendors.cf5d6274.js"
+  },
+  {
+    "revision": "c87be2ec60ada205b98f8d7d3af041cb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
